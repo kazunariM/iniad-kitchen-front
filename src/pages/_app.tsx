bbd871ae6b915b7718544ed3800eed89b2e0145d
Kazunari MATSUMOTO <s1f102003672@iniad.org>
2024-11-16 19:21:45 +0900

    バックエンドとの接続(作りかけ)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,68 @@
 import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import App, { AppContext, AppProps } from 'next/app'
 import Head from 'next/head'
+import axios, { AxiosResponse } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Head>
-        <link rel="icon" type="image/png" href="/kITchen.png" />
-        <title key="title">INIAD kITchen</title>
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content="INIADサークル INIAD kITchen" key="og_title" />
-        <meta property="og:description" content="INIAD公認サークル「INIAD kITchen」のホームページです。" key="og_description" />
-        <meta property="og:url" content="https://iniad-kitchen.com" key="og_url" />
-        <meta property="og:site_name" content="INIAD kITchen Official Site" />
-        <meta property="og:image" content="https://iniad-kitchen.com/kITchen.png" key="og_image" />
-        <meta name="twitter:card" content="summary" key="og_twitter" />
-        <meta name="twitter:site" content="INIAD_kITchen" />
-    </Head>
-    <Component {...pageProps} />
-  </>
+interface PageProps {
+  is_active: boolean,
+  can_publish: boolean
 }
+
+const instance = axios.create({
+  baseURL: process.env.NODE_ENV === 'production'
+      ? 'https://iniad-kitchen.com/api/staff/'
+      : 'http://localhost:8000/api/staff/',
+})
+
+class MyApp extends App<AppProps> {
+  static async getInitialProps({ Component, ctx }: AppContext): Promise<{ pageProps: PageProps }> {
+    let pageProps: PageProps = { is_active: false, can_publish: false }
+    const cookies = parseCookies(ctx)
+    setCookie(ctx, 'sessionid', cookies.sessionid)
+    
+    if (ctx.pathname.startsWith('/staff/')) {
+      try {
+          const response: AxiosResponse<{ is_active: false, can_publish: false }> = await instance.get('/check/', {
+            headers: {
+              cookie: `sessionid=${cookies.sessionid}`,
+            },
+            withCredentials: true
+          })
+          const authenticated = response.data.is_active  
+          if (!authenticated && ctx.res) {
+            ctx.res.writeHead(302, { Location: '/' })
+            ctx.res.end()
+          }
+      } catch (error) {
+        if (ctx.res) {
+          ctx.res.writeHead(302, { Location: '/' })
+          ctx.res.end()
+        }
+      }
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return <>
+      <Head>
+          <link rel="icon" type="image/png" href="/kITchen.png" />
+          <title key="title">INIAD kITchen</title>
+          <meta property="og:type" content="article" />
+          <meta property="og:title" content="INIADサークル INIAD kITchen" key="og_title" />
+          <meta property="og:description" content="INIAD公認サークル「INIAD kITchen」のホームページです。" key="og_description" />
+          <meta property="og:url" content="https://iniad-kitchen.com" key="og_url" />
+          <meta property="og:site_name" content="INIAD kITchen Official Site" />
+          <meta property="og:image" content="https://iniad-kitchen.com/kITchen.png" key="og_image" />
+          <meta name="twitter:card" content="summary" key="og_twitter" />
+          <meta name="twitter:site" content="INIAD_kITchen" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  }
+}
+
+export default MyApp
--- src/pages/staff/article/list.tsx
+++ src/pages/staff/article/list.tsx
@@ -2,33 +2,40 @@ import Head from 'next/head'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Link from 'next/link';
+import { parseCookies } from 'nookies'
 
 const instance = axios.create({
+    withCredentials: true,
     baseURL: process.env.NODE_ENV === 'production'
         ? 'https://iniad-kitchen.com/api/staff/'
         : 'http://localhost:8000/api/staff/',
 });
 
-export default function StaffArticleList() {
-    const [articles, setArticles] = useState<Array<any> | null>(null)
+type Props = {
+    data: any,
+}
 
-    useEffect(() => {
-        async function fetchData() {
-            const res = await instance.get('/articles/')
-            setArticles(res.data)
-        }
+export default function StaffArticleList(props: Props) {
+    // const [articles, setArticles] = useState<Array<any> | null>(null)
 
-        fetchData()
-    }, [])
+    // useEffect(() => {
+    //     const cookies = parseCookies()
+    //     async function fetchData() {
+    //         const res = await instance.get('/articles/')
+    //         setArticles(res.data)
+    //     }
 
-    if (articles) {
-        const items = [<p>tset</p>]
+    //     fetchData()
+    // }, [])
+
+    if (props.data) {
+        const items = []
         let data: any
-        for (data of articles) {
+        for (data of props.data) {
             items.push(
                 <article>
                     <div>
-                        <h3><Link href={`/editor/${data.uuid}/`}>{"> "}{data.title}</Link></h3>
+                        <h3><Link href={`editor/${data.uuid}/`}>{"> "}{data.title}</Link></h3>
                         <small>{data.event_day}</small>
                     </div>
                     <div dangerouslySetInnerHTML={{ __html: data.text }}></div>
@@ -38,5 +45,22 @@ export default function StaffArticleList() {
         return items
     } else {
         return (<></>)
+    }
+}
+
+export async function getServerSideProps() {
+    const cookies = parseCookies()
+    try {
+        const res = await instance.get(`/articles/`, {
+            headers: {
+              cookie: `sessionid=${cookies.sessionid}`,
+            },
+          }
+        )
+        const data = res.data
+        return { props: { data } }
+    } catch (error) {
+        const data = {}
+        return { props: { data } }
     }
 }
